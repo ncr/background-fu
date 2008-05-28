c45a30cf5ff404bd25005da70c88bb54bb6b32c3
Jacek Becela <jacek.becela@gmail.com>
2008-05-28 19:36:10 +0200

    init.rb and rails/init.rb for plugins and gems

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -1,5 +1,5 @@
 module BackgroundFu
   
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
   
 end
--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,10 @@
+require 'background_fu'
+require 'background_fu/worker_monitoring'
+require 'job'
+
 Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
 
 if ActiveRecord::Base.allow_concurrency
+  require 'job/bonus_features'
   Job.send!(:include, Job::BonusFeatures)
 end
