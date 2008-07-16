5576c32bbb2cb6bd6d6e6ce8ff2f2b846629426d
Jacek Becela <jacek.becela@gmail.com>
2008-07-16 11:35:34 +0200

    Revert to Dependencies.load_paths

--- rails/init.rb
+++ rails/init.rb
@@ -3,7 +3,8 @@ require 'background_fu/worker_monitoring'
 require 'job'
 require 'job/bonus_features'
 
-ActiveSupport::Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
+# ActiveSupport::Dependencies will be here after edge becomes stable.
+Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
 
 if ActiveRecord::Base.allow_concurrency
   Job.send!(:include, Job::BonusFeatures)
