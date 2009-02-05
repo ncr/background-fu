9868a0bb57a7416b62c72e615e4764cbffc5285d
Oliver Beddows <oliver.beddows@googlemail.com>
2009-02-05 20:24:44 +0800

    Fixed deprecated call to Dependencies.load_paths.
    Signed-off-by: Jacek Becela <jacek.becela@gmail.com>

--- rails/init.rb
+++ rails/init.rb
@@ -3,8 +3,7 @@ require 'background_fu/worker_monitoring'
 require 'job'
 require 'job/bonus_features'
 
-# ActiveSupport::Dependencies will be here after edge becomes stable.
-Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
+ActiveSupport::Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
 
 if ActiveRecord::Base.method_defined?(:allow_concurrency) and ActiveRecord::Base.allow_concurrency
   Job.send!(:include, Job::BonusFeatures)
