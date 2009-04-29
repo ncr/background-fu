e83f775e448fda24b2df3140576faa9d1d74671e
Brandt Kurowski <kurowski@logicbranch.com>
2009-04-30 03:12:07 +0800

    Fix for non-existent config file
    Signed-off-by: Jacek Becela <jacek.becela@gmail.com>

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -1,7 +1,8 @@
 module BackgroundFu
   
   VERSION = "1.0.10"
-  CONFIG = YAML::load(File.open("#{RAILS_ROOT}/config/daemons.yml"))['background_fu'] || {}
+  CONFIG_FILE = "#{RAILS_ROOT}/config/daemons.yml"
+  CONFIG = File.exist?(CONFIG_FILE) && YAML::load_file(CONFIG_FILE)['background_fu'] || {}
   CONFIG['cleanup_interval'] ||= :on_startup
   CONFIG['monitor_interval'] ||= 10
   
