8822b81078f4c3c4922fc9ed57697b320ccac645
sk <opencomputing@gmail.com>
2009-05-18 17:18:07 +0800

    patched bonus include depending on rails version
    Signed-off-by: Jacek Becela <jacek.becela@gmail.com>

--- rails/init.rb
+++ rails/init.rb
@@ -5,6 +5,7 @@ require 'job/bonus_features'
 
 ActiveSupport::Dependencies.load_paths << "#{RAILS_ROOT}/lib/workers"
 
-if ActiveRecord::Base.method_defined?(:allow_concurrency) and ActiveRecord::Base.allow_concurrency
+#ref    http://guides.rubyonrails.org/2_2_release_notes.html says 2.2 onwards allow_concurrency deprecated
+if (Rails::VERSION::STRING >= '2.2') or (ActiveRecord::Base.method_defined?(:allow_concurrency) and ActiveRecord::Base.allow_concurrency)
   Job.send(:include, Job::BonusFeatures)
 end
