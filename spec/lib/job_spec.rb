5d5de7d1f5285455cd5ef37b046b6a7fae6f98cd
jrwest <jordan.west@westwardwd.com>
2010-02-21 02:02:57 -0500

    implemented specs for Job#enqueue! and initialization
    Still need to implement the invocation specs and write many others

--- spec/lib/job_spec.rb
+++ spec/lib/job_spec.rb
@@ -1,10 +1,49 @@
 require File.join(File.dirname(__FILE__), %w[.. spec_helper])
 
 describe Job do 
+  before(:each) do
+    @job = Job.enqueue!(ExampleWorker, :add, 1, 2)
+  end
   #TODO DRY this up. Can't get the shoulda macros working
   specify { subject.should validate_presence_of :worker_class }
   specify { subject.should validate_presence_of :worker_method }
   specify { subject.should have_readonly_attribute :worker_class }
   specify { subject.should have_readonly_attribute :worker_method }
   specify { subject.should have_readonly_attribute :args }
+  describe "#enqueue!" do
+    it "returns an instance of Job containing the given details" do
+      @job.should be_instance_of Job
+      @job.worker_class.constantize.should == ExampleWorker
+      @job.worker_method.to_sym.should == :add
+      @job.args.should == [1, 2]
+    end
+    it "stores a new job" do
+      stored = Job.find(@job.id)
+      @job.should == stored
+    end
+  end
+  describe "running" do
+    context "initialization" do
+      it "sets the start time" do
+        lambda { 
+          @job.initialize_worker
+        }.should change{@job.started_at}
+        
+      end
+      it "sets the state to 'running'" do
+        lambda {
+          @job.initialize_worker
+        }.should change{@job.state}.to("running")
+      end
+      it "instantiates a worker to 'run'" do
+        @job.worker_class.constantize.should_receive(:new)
+        @job.initialize_worker
+      end
+    end
+    context "invocation" do
+      it "calls the worker method on the given worker with the given args"
+      it "stores the result of the worker call"
+      it "changes the state to 'finished'"
+    end
+  end
 end
