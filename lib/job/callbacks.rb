0bdee695be1bb609cca6c8fab22a20fd9bc55676
jrwest <jordan.west@westwardwd.com>
2010-02-20 19:08:03 -0500

    the basics of callbacks (before_invoke)
    before_invoke callback allows methods in the Job class to be called right before it is run. Use the macro before_invoke(*do_methods) to specify which methods to call. The callback chain does not interrupt the running of the job. It is arrogant of the results returned by each before_invoke method. Methods are called in the order they are added so
    	
    	before_invoke :method1, :method2, :method3
    
    is the same as
    
    	before_invoke :method1
    	before_invoke :method2
    	before_invoke :method3

--- lib/job/callbacks.rb
+++ lib/job/callbacks.rb
@@ -1,10 +1,56 @@
 module Job::Callbacks
   def self.included(base)
+    base.alias_method_chain :invoke_worker, :callbacks
     base.extend(ClassMethods)
   end
 
-  module ClassMethods
-    def callbacks
+  def invoke_worker_with_callbacks
+    call_before_invokes
+    invoke_worker_without_callbacks
+  end
+
+  private
+
+    def call_before_invokes
+      if has_before_invokes?
+        self.class.callbacks[:invoke][:before].each do |method|
+          self.send(method)
+        end
+      end
     end
+
+    def has_before_invokes?
+      if invokes = self.class.callbacks[:invoke]
+        return invokes.has_key? :before
+      end 
+    end
+
+  module ClassMethods
+    
+    def before_invoke(*do_methods)
+      do_methods.each do |method|
+        set_callback(:invoke, :before, method)
+      end
+    end
+
+    def callbacks
+      @callbacks ||= {}
+    end
+
+    def clear_callbacks!
+      @callbacks = {}
+    end
+
+    private 
+      
+      #callbacks are stored as a hash 
+      #organized by method and when(before, after)
+      #each for_method, when pair holds an array
+      #of methods to call
+      def set_callback(for_method, whn, do_method)
+        callbacks[for_method] ||= {}
+        callbacks[for_method][whn] ||= []
+        callbacks[for_method][whn] << do_method
+      end
   end
 end
--- spec/lib/job/callbacks_spec.rb
+++ spec/lib/job/callbacks_spec.rb
@@ -2,10 +2,48 @@ require File.join(File.dirname(__FILE__), %w[.. .. spec_helper])
 
 describe Job, "Callbacks" do
   subject { Job }
-  let(:some_instance) { mock(Job).as_null_object }
-  it "has a class method callbacks that returns the callback store" do 
-    Job.should respond_to :callbacks
+  let(:example_worker) { mock("Some Worker", :some_method => "a result") }
+  let(:some_instance) do
+    unless job = Job.find(:first)
+      job = Job.enqueue!(example_worker, :some_method)
+    end
+    3.times do |i|
+      job.stub!("method#{i+1}").and_return(nil)
+    end
+    job.instance_variable_set("@worker", example_worker)
+    job
   end
-  it "has a hash-like accessible store"
-  it "can clear the callbacks store"
+  it "has a class method callbacks that returns the callback store" do 
+    subject.should respond_to :callbacks
+  end
+  it "has a hash-like accessible store" do 
+    subject.callbacks.should respond_to :[]
+  end
+  it "can clear the callbacks store" do
+    subject.clear_callbacks!
+    subject.callbacks.should == {}
+  end
+  describe "before_invoke" do    
+    before(:each) do
+      subject.clear_callbacks!
+    end
+    it "takes a symbol representing a method to call" do
+      subject.send(:before_invoke, :method1)
+    end
+    it "takes a list of symbols representing methods to call" do
+      subject.send(:before_invoke, :method1, :method2)
+    end
+    describe "callback" do
+      let(:cb_methods) { [:method1, :method2, :method3] }
+      before(:each) do
+        subject.send(:before_invoke, *cb_methods)
+      end
+      it "calls each method in order" do
+        cb_methods.each do |method|
+          some_instance.should_receive(method).ordered
+        end
+        some_instance.invoke_worker
+      end      
+    end
+  end
 end
