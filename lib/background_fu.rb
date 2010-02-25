1adeebaa17d4a792485d19783d39e9edb641e356
jrwest <jordan.west@westwardwd.com>
2010-02-25 17:56:39 -0500

    RAILS_ROOT is deprecated so use Rails.root if its available

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -1,7 +1,7 @@
 module BackgroundFu
   
   VERSION = "1.0.10"
-  CONFIG_FILE = "#{RAILS_ROOT}/config/daemons.yml"
+  CONFIG_FILE = "#{Rails.respond_to?(:root) ? Rails.root : RAILS_ROOT}/config/daemons.yml"
   CONFIG = File.exist?(CONFIG_FILE) && YAML::load_file(CONFIG_FILE)['background_fu'] || {}
   CONFIG['cleanup_interval'] ||= :on_startup
   CONFIG['monitor_interval'] ||= 10
