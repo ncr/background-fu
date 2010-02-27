825077bb5c0e0a013d31f7184a826cb03bc6daa9
jrwest <jordan.west@westwardwd.com>
2010-02-26 23:01:36 -0500

    Background-Fu config file is now config/background.rb instead of config/daemons.rb. No longer use the rails initialization process load workers.
    The workers are just required if included in lib/workers. There is no reason to add the the load path when lib dir is already in it. Also, this will automatically require workers in the background script.  
    
    The Railtie at this point is empty and could be done away with but since we will be using Rails::Engine soon I have just left the file.

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -1,7 +1,7 @@
 module BackgroundFu
   
   VERSION = "1.0.10"
-  CONFIG_FILE = "#{Rails.respond_to?(:root) ? Rails.root : RAILS_ROOT}/config/daemons.yml"
+  CONFIG_FILE = "config/background.yml"
   CONFIG = File.exist?(CONFIG_FILE) && YAML::load_file(CONFIG_FILE)['background_fu'] || {}
   CONFIG['cleanup_interval'] ||= :on_startup
   CONFIG['monitor_interval'] ||= 10
@@ -11,5 +11,5 @@ end
 require 'job'
 require 'background_fu/worker_monitoring'
 require 'background_fu/railtie' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-
+Dir["lib/workers/*"].each { |f| require f } 
 
--- lib/background_fu/railtie.rb
+++ lib/background_fu/railtie.rb
@@ -1,13 +1,9 @@
 require "background_fu"
 require "rails"
-require "rails/engine" #needed for set_load_path initializer
 
 module BackgroundFu
   class Railtie < Rails::Railtie
     railtie_name :background_fu
 
-    initializer "background_fu.set_load_paths", :after => :set_load_path do |app|
-      app.config.load_paths += %w('lib/workers')
-    end
   end
 end
